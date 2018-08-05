e462a6ff8bc84bf83eaf38e3468bf1ccc9a75afe
Cesar <cesar777welch@gmail.com>
2018-08-05 00:11:52 -0600

    3 in a row

--- server/controllers/guest.js
+++ server/controllers/guest.js
@@ -16,7 +16,7 @@ module.exports = {
     updateIds(req, res) {
         return Guest.all().then(guests => {
             for (var i = 0; i < guests.length; i++) {
-              guests[i].dataValues = md5(guests[i].dataValues.id);
+              guests[i].dataValues;.binaryId = md5(guests[i].dataValues.id);
             }
             res.status(200).send(guests);
         }).catch(error => res.status(400).send(error));
