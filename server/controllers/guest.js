0b307b6a8cae054b58838b206b90e50a90a19b98
Cesar <cesar777welch@gmail.com>
2018-08-04 22:05:33 -0600

    changes to endpoints

--- server/controllers/guest.js
+++ server/controllers/guest.js
@@ -1,22 +1,24 @@
 const Guest = require('../models').guest;
-
+var md5 = require('md5');
 module.exports = {
-  create(req, res) {
-    return Guest
-      .create({
-        name: req.body.name,
-        email: req.body.email,
-        plusone: req.body.plusone,
-        plusonelist: req.body.plusonelist,
-        binaryId: req.body.binaryId
-      })
-      .then(guest => res.status(201).send(guest))
-      .catch(error => res.status(400).send(error));
-  },
-  list(req, res) {
-  return Guest
-    .all()
-    .then(guests => res.status(200).send(guests))
-    .catch(error => res.status(400).send(error));
-  },
+    create(req, res) {
+        return Guest.create({
+            name: req.body.name,
+            email: req.body.email,
+            plusone: req.body.plusone,
+            plusonelist: req.body.plusonelist,
+            binaryId: req.body.binaryId
+        }).then(guest => res.status(201).send(guest)).catch(error => res.status(400).send(error));
+    },
+    list(req, res) {
+        return Guest.all().then(guests => res.status(200).send(guests)).catch(error => res.status(400).send(error));
+    },
+    updateIds(req, res) {
+        return Guest.all().then(guests => {
+            for (var i = 0; i < guests.length; i++) {
+                guests[i].dataValues.binaryId = md5(guests[i].dataValues.id);
+            }
+            res.status(200).send(guests);
+        }).catch(error => res.status(400).send(error));
+    }
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -8,4 +8,5 @@ module.exports = (app) => {
 
   app.post('/api/guest', guestsController.create);
   app.get('/api/guest', guestsController.list);
+  app.get('/api/updateIds', guestsController.updateIds);
 };
